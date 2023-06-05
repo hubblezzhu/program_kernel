befecc409478cb0f70ce050eaf9cab9fcf8d296f
Super User <root@openEuler_23_03_1.smartont.net>
2023-06-05 15:47:00 +0800

    WIP
    Signed-off-by: Super User <root@openEuler_23_03_1.smartont.net>

--- eBPF_sockmap/sockops.h
+++ eBPF_sockmap/sockops.h
@@ -2,18 +2,20 @@
 
 struct sock_key
 {
-   __u32 sip;    //源IP
-   __u32 dip;    //目的IP
-   __u32 sport;  //源端口
-   __u32 dport;  //目的端口
-   __u32 family; //协议
+    __u32 sip;    //源IP
+    __u32 dip;    //目的IP
+    __u32 sport;  //源端口
+    __u32 dport;  //目的端口
+    __u32 family; //协议
 };
 
 
-struct bpf_map_def SEC("maps") sock_ops_map = {
-  .type = BPF_MAP_TYPE_SOCKHASH,
-  .key_size = sizeof(struct sock_key),
-  .value_size = sizeof(int),
-  .max_entries = 65535,
-  .map_flags = 0,
-};
+struct {
+    __uint(type, BPF_MAP_TYPE_SOCKHASH);
+    __uint(key_size, sizeof(struct sock_key));
+    __uint(value_size, sizeof(int));
+    __uint(max_entries, 65535);
+    __uint(map_flags, 0);
+} sock_ops_map SEC(".maps");
+
+
