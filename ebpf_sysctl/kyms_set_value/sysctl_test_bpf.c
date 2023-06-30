41c9f58317e028ed858441475108b5f8a123a213
Zhu Hengbo <hubble_zhu@qq.com>
2023-06-30 15:21:50 +0800

    Update kyms
    Signed-off-by: Zhu Hengbo <hubble_zhu@qq.com>

--- ebpf_sysctl/kyms_set_value/sysctl_test_bpf.c
+++ ebpf_sysctl/kyms_set_value/sysctl_test_bpf.c
@@ -23,21 +23,56 @@
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #endif
 
-const char symbol_name[] = "sysctl_tcp_mem111";
+const char tcp_mem_name[] = "net/ipv4/tcp_mem";
+const char symbol_name[] = "sysctl_tcp_mem";
+
+static __always_inline int is_tcp_mem(struct bpf_sysctl *ctx)
+{
+    unsigned char i;
+    char name[sizeof(tcp_mem_name)];
+    int ret;
+
+    memset(name, 0, sizeof(name));
+    ret = bpf_sysctl_get_name(ctx, name, sizeof(name), 0);
+    if (ret < 0 || ret != sizeof(tcp_mem_name) - 1)
+        return 0;
+
+#pragma clang loop unroll(full)
+    for (i = 0; i < sizeof(tcp_mem_name); ++i)
+        if (name[i] != tcp_mem_name[i])
+            return 0;
+
+    return 1;
+}
+
+
+struct Fake_tcp_mem {
+    unsigned long low_mem;
+    unsigned long middle_mem;
+    unsigned long high_mem;
+};
+ // * long bpf_kallsyms_lookup_name(const char *name, int name_sz, int flags, u64 *res)
 
 SEC("cgroup/sysctl")
 int sysctl_test(struct bpf_sysctl *ctx)
 {
     unsigned long target_tcp_mem[3] = {117015, 156022, 234031};
-    volatile int ret;
-    int name_size = 0;
+    unsigned long tmp_tcp_mem[3] = {0, 0, 0};
+    char value[MAX_VALUE_STR_LEN];
 
-    bpf_printk("Entering sysctl ebpf hook...\n");
-    if (!ctx->write)
+    volatile int ret;
+
+    int name_size = 0;
+    int flags = 0;
+    unsigned long addr = 0;
+
+    if (ctx->write)
         return 0;
 
-    // if (!is_tcp_mem(ctx))
-    //     return 0;
+    if (!is_tcp_mem(ctx))
+        return 0;
+
+    bpf_printk("Entering sysctl ebpf hook...\n");
 
 //     ret = bpf_sysctl_get_current_value(ctx, value, MAX_VALUE_STR_LEN);
 //     if (ret < 0 || ret >= MAX_VALUE_STR_LEN)
@@ -54,6 +89,24 @@ int sysctl_test(struct bpf_sysctl *ctx)
 
 //     return tcp_mem[0] < tcp_mem[1] && tcp_mem[1] < tcp_mem[2];
 
+    ret = bpf_sysctl_get_current_value(ctx, value, MAX_VALUE_STR_LEN);
+    if (ret < 0 || ret >= MAX_VALUE_STR_LEN)
+        return 0;
+
+    name_size = sizeof(symbol_name) / sizeof(char);
+    ret = bpf_kallsyms_lookup_name(symbol_name, name_size, flags, (unsigned long long *)&addr);
+    if (ret < 0) {
+        bpf_printk("bpf_kallsyms_lookup_name failed. error %d\n", ret);
+        return 0;
+    }
+
+    if (addr == 0) {
+        bpf_printk("symbol %s not found. \n", symbol_name);
+        return 0;
+    }
+
+    bpf_printk("symbol %s addr %lx\n", symbol_name, addr);
+
     bpf_printk("start set ksyms value.\n");
     name_size = sizeof(symbol_name) / sizeof(char);
     ret = bpf_kyms_set_value(symbol_name, name_size, (char *)target_tcp_mem, sizeof(long) * 3);
@@ -64,7 +117,8 @@ int sysctl_test(struct bpf_sysctl *ctx)
 
     bpf_printk("ksyms value set down.\n");
 
-    return 0;
+
+    return 1;
 }
 
 
